11c272e4fc38f379f43eff5b598d5aed0d1c3938
Sridhar Ratnakumar <srid@srid.ca>
2021-01-26 22:02:22 -0500

    Try haskell.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,23 @@
-{ pkgs ? import ./dep/nixpkgs {} }:
-let 
-  inherit (import ./dep/gitignoresrc { inherit (pkgs) lib; }) gitignoreSource;
-in 
-  pkgs.haskellPackages.developPackage {
-    name = "g";
-    root = gitignoreSource ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-        [ cabal-install
-          cabal-fmt
-          ghcid
-          haskell-language-server
-        ]);
-  }
+{ # Fetch the latest haskell.nix and import its default.nix
+  haskellNix ? import ./dep/haskell.nix {}
+
+# haskell.nix provides access to the nixpkgs pins which are used by our CI,
+# hence you will be more likely to get cache hits when using these.
+# But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+
+# haskell.nix provides some arguments to be passed to nixpkgs, including some
+# patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+
+# import nixpkgs with overlays
+, pkgs ? import nixpkgsSrc nixpkgsArgs
+}: pkgs.haskell-nix.project {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "haskell-nix-project";
+    src = ./.;
+  };
+  # Specify the GHC version to use.
+  compiler-nix-name = "ghc8102"; # Not required for `stack.yaml` based projects.
+}
