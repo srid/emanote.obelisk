3b8f48f61839957f5c30940678c3499c281b5ec5
Sridhar Ratnakumar <srid@srid.ca>
2021-01-30 18:37:54 -0500

    Don't dump graph (slow on larger zks)

--- src/G/WebServer.hs
+++ src/G/WebServer.hs
@@ -9,7 +9,6 @@ import G.Markdown.WikiLink (WikiLinkID)
 import qualified Network.Wai.Middleware.Static as MStatic
 import Reflex.Dom.Core
 import qualified Reflex.Dom.Pandoc as PR
-import qualified Shower
 import Text.Pandoc.Definition (Pandoc)
 import Web.Scotty (html, param, scotty)
 import qualified Web.Scotty as Scotty
@@ -40,7 +39,7 @@ run inputDir Db {..} = do
               html $ decodeUtf8 s
 
 vertexWidget :: (PR.PandocBuilder t m) => Zk -> WikiLinkID -> Pandoc -> m ()
-vertexWidget Zk {..} wlId doc = do
+vertexWidget _zk wlId doc = do
   -- NOTE: Aim to use semantic tags, and let the user style it via custom CSS (not Clay)
   -- TODO: Read https://css-tricks.com/how-to-section-your-html/
   el "html" $ do
@@ -58,4 +57,5 @@ vertexWidget Zk {..} wlId doc = do
             el "h2" $ text "Backlinks"
             text "TODO: backlinks"
         el "footer" $ do
-          el "pre" $ text $ toText $ Shower.shower _zk_graph
+          -- el "pre" $ text $ toText $ Shower.shower _zk_graph
+          text "github.com/srid/g"
