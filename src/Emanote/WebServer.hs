d73814569eb7fadc5bdb1945b767f6bc9cd63681
Sridhar Ratnakumar <srid@srid.ca>
2021-01-31 12:29:04 -0500

    Display backlinks

--- src/Emanote/WebServer.hs
+++ src/Emanote/WebServer.hs
@@ -6,12 +6,14 @@ import qualified Data.Text as T
 import Emanote.Db (Db (..))
 import Emanote.Db.Types.Zk (Zk (..))
 import Emanote.Db.Types.Zk.Patch (ZkPatch)
-import Emanote.Markdown.WikiLink (WikiLinkID)
+import qualified Emanote.Graph as G
+import Emanote.Markdown.WikiLink (WikiLinkContext, WikiLinkID, WikiLinkLabel)
 import qualified Network.Wai.Middleware.Static as MStatic
 import Reflex.Dom.Builder.Static (renderStatic)
 import qualified Reflex.Dom.Pandoc as PR
 import Text.Mustache (ToMustache, object, (~>))
 import qualified Text.Mustache as Mustache
+import Text.Pandoc.Definition (Pandoc (Pandoc))
 import Web.Scotty (html, param, scotty)
 import qualified Web.Scotty as Scotty
 
@@ -47,17 +49,47 @@ run inputDir Db {..} = do
                 Just (Left err) -> Scotty.text $ "oopsy template: " <> show err
                 Just (Right tmpl) -> do
                   mdHtml <- fmap snd $ liftIO $ renderStatic $ PR.elPandoc PR.defaultConfig doc
-                  let s = Mustache.substitute tmpl (Page wikiLinkID mdHtml)
+                  backlinks <-
+                    liftIO $
+                      fmap concat $
+                        forM (G.preSetWithLabel wikiLinkID (G.unGraph _zk_graph)) $ \(es, t) ->
+                          forM es $ \e ->
+                            mkBacklink e t
+                  let page = Page wikiLinkID mdHtml backlinks
+                      s = Mustache.substitute tmpl page
                   html $ toLazy s
+
+data Backlink = Backlink
+  { _backlink_id :: WikiLinkID,
+    _backlink_label :: WikiLinkLabel,
+    _backlink_ctxHtml :: Text
+  }
+
+mkBacklink :: (WikiLinkLabel, WikiLinkContext) -> WikiLinkID -> IO Backlink
+mkBacklink (_backlink_label, ctx) _backlink_id = do
+  -- TODO: Create backlinks as Incremental, to avoid rendering on demand.
+  _backlink_ctxHtml :: Text <-
+    fmap (decodeUtf8 . snd) $ liftIO $ renderStatic $ PR.elPandoc PR.defaultConfig $ Pandoc mempty ctx
+  pure $ Backlink {..}
 
 data Page = Page
   { _page_wikiLinkID :: WikiLinkID,
-    _page_mdHtml :: ByteString
+    _page_mdHtml :: ByteString,
+    _page_backlinks :: [Backlink]
   }
+
+instance ToMustache Backlink where
+  toMustache Backlink {..} =
+    object
+      [ "id" ~> untag _backlink_id,
+        "label" ~> show @Text _backlink_label,
+        "ctxHtml" ~> _backlink_ctxHtml
+      ]
 
 instance ToMustache Page where
   toMustache Page {..} =
     object
       [ "wikiLinkID" ~> untag _page_wikiLinkID,
-        "mdHtml" ~> decodeUtf8 @Text _page_mdHtml
+        "mdHtml" ~> decodeUtf8 @Text _page_mdHtml,
+        "backlinks" ~> _page_backlinks
       ]
