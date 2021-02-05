3eeeb841e4d11d512891d39909ff6b2e1bbe9540
Sridhar Ratnakumar <srid@srid.ca>
2021-02-04 19:12:29 -0500

    normalize dep

--- emanote-cli/default.nix
+++ emanote-cli/default.nix
@@ -1,18 +1,18 @@
-{ pkgs ? import ./dep/nixpkgs {} }:
+{ pkgs ? import ../dep/nixpkgs {} }:
 let 
-  inherit (import ./dep/gitignoresrc { inherit (pkgs) lib; }) gitignoreSource;
+  inherit (import ../dep/gitignoresrc { inherit (pkgs) lib; }) gitignoreSource;
   hp = (pkgs.haskellPackages.override {
     overrides = self: super: with pkgs.haskell.lib; {
       reflex-fsnotify = 
         doJailbreak 
           (self.callCabal2nix "reflex-fsnotify" 
-            (import ./dep/reflex-fsnotify/thunk.nix) {});
+            (import ../dep/reflex-fsnotify/thunk.nix) {});
     };
   }).extend (pkgs.haskell.lib.packageSourceOverrides {
     emanote-core = gitignoreSource ./../emanote-core;
     emanote-cli = gitignoreSource ./.;
-    reflex-dom-pandoc = import ./dep/reflex-dom-pandoc/thunk.nix;
-    pandoc-link-context = import ./dep/pandoc-link-context/thunk.nix;
+    reflex-dom-pandoc = import ../dep/reflex-dom-pandoc/thunk.nix;
+    pandoc-link-context = import ../dep/pandoc-link-context/thunk.nix;
   });
   shell = hp.shellFor {
     packages = p: [ p.emanote-cli ];
